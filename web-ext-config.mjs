bd7d493ef4439ec08fc0d298f8c53116b17525d5
Zook <10180136+doctorzook@users.noreply.github.com>
2026-06-14 14:35:23 -0700

    Basic build infrastructure

---
+++ web-ext-config.mjs
@@ -0,0 +1,12 @@
+export default {
+  ignoreFiles: [
+    "package.json",
+    "package-lock.json",
+    "node_modules",
+    "web-ext-config.mjs",
+    "README.md",
+    "PRIVACY.md",
+    "LICENSE",
+    ".gitignore",
+  ],
+};
